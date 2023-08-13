7d5fe64ede5dd7f2f9c87e04ba6a3c7e17ec24d4
MARUTHI R <80270578+maruthi-syn-ack@users.noreply.github.com>
2023-08-13 23:47:45 +0530

    Time: 1600 ms (23.27%) | Memory: 0B (100.00%) - LeetSync

--- 1153-product-sales-analysis-i/product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/product-sales-analysis-i.sql
@@ -1,4 +1,5 @@
 /* Write your PL/SQL query statement below */
 
-Select  p.product_name, s.year,  s.price 
-from Sales s , Product p where s.product_id = p.product_id;
+-- Select  p.product_name, s.year,  s.price 
+-- from Sales s , Product p where s.product_id = p.product_id;
+SELECT P.product_name ,S.year  ,S.price FROM Sales  S LEFT JOIN Product P ON S.product_id =P.product_id 
